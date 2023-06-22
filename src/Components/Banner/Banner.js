0a3fd96d29793d40b53069199def5472ba791149
Ashmit k <ashmithkacheri138@gmail.com>
2023-06-22 19:31:23 +0530

    Icons added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {API_KEY,imageUrl} from '../../constants/constants'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay ,faPlus} from '@fortawesome/free-solid-svg-icons';
 import './Banner.css'
 import axios from '../../axios'
 
@@ -22,8 +24,8 @@ function Banner() {
             <div className='content'>
                 <h1 className='title'>{movie ?movie.name:""}</h1>
                 <div className='banner-buttons'>
-                    <button className='button'>Play</button>
-                    <button className='button'>My list</button>
+                    <button className='button'><FontAwesomeIcon icon={faPlay} className='faplay'/>Play</button>
+                    <button className='button'><FontAwesomeIcon icon={faPlus} className='faplus'/>My list</button>
                 </div>
                 <h1 className='description'>{movie ?movie.overview:""}</h1>
             </div>
